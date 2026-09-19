30490d043af9c03e70fc0b22e8d5d67d2e564682
4rthurbirl35 <4rthurbezerra2007@gmail.com>
2026-09-18 22:32:46 -0300

    Add discount calculation based on quantity

---
+++ desconto_quantidade.php
@@ -0,0 +1,15 @@
+<?php
+
+$quantidade = 7;
+
+if ($quantidade >= 10) {
+    echo "Desconto: 20%";
+
+} elseif ($quantidade >= 5){
+    echo "desconto: 10%";
+} else {
+    echo "sem desconto";
+
+}
+
+?>
